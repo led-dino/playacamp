8d9c579777105e669ee51c28e704db950407de91
Mark Hahnenberg <mhahnenb@gmail.com>
2017-08-15 09:44:36 -0700

    Fix remaining old imports

--- playacamp/urls.py
+++ playacamp/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import main.views
 
 # Examples:
 # url(r'^$', 'playacamp.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^$', main.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
