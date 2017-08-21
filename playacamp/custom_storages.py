92114da4d5486cb8ad6be6130d325537f83529be
Mark Hahnenberg <mark@nylas.com>
2017-08-21 16:46:10 -0700

    Add AttendanceProfile model and friends

--- playacamp/custom_storages.py
+++ playacamp/custom_storages.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3BotoStorage):  # pylint: disable=abstract-method
     location = settings.MEDIAFILES_LOCATION
-
