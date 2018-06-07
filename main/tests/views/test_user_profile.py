35d931fa3e1bf5c353ec2bec93077189edfbb509
Mark Hahnenberg <mark@nylas.com>
2018-06-07 09:44:56 -0700

    Fix HTTPS redirect checking in unit tests

--- main/tests/views/test_user_profile.py
+++ main/tests/views/test_user_profile.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
 
 from main.models import UserProfile
+
+
+def get_absolute_url_from_relative(relative_url: str) -> str:
+    fake_request = RequestFactory().post('/foobar', follow=True, secure=True)
+    return fake_request.build_absolute_uri(relative_url)
 
 
 class TestUserProfileViews(TestCase):
@@ -17,10 +22,11 @@ class TestUserProfileViews(TestCase):
 
     def test_change_attending_view(self):
         self.client.login(username='foobar', password='foobarbaz')
+        expected_redirect_url = get_absolute_url_from_relative(reverse('user-profile-me'))
         response = self.client.post(reverse('changed-attending'), {
             'is-attending': 'on',
         }, follow=True, secure=True)
-        self.assertRedirects(response, reverse('user-profile-me'))
+        self.assertRedirects(response, expected_redirect_url)
         attendance = self.user_profile.try_fetch_current_attendance()
         self.assertIsNotNone(attendance)
         self.assertFalse(attendance.deleted_at)
@@ -28,6 +34,6 @@ class TestUserProfileViews(TestCase):
         response = self.client.post(reverse('changed-attending'), {
             'is-attending': 'off',
         }, follow=True, secure=True)
-        self.assertRedirects(response, reverse('user-profile-me'))
+        self.assertRedirects(response, expected_redirect_url)
         attendance = self.user_profile.try_fetch_current_attendance()
         self.assertTrue(attendance.deleted_at)
