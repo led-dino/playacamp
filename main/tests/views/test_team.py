1869f556ea485d02d001926bdafa8961757c6ebc
Mark Hahnenberg <mark@nylas.com>
2018-06-10 10:38:46 -0700

    Add tests for team endpoints

---
+++ main/tests/views/test_team.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from main.models import Team, UserProfile
+
+
+class TestTeamView(TestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username='foobar',
+                                             email='foobar@gmail.com',
+                                             password='foobarbaz')
+        self.user_profile = UserProfile()
+        self.user_profile.user = self.user
+        self.user_profile.save()
+
+        self.team = Team(name='Kitchen', description='Cook stuff')
+        self.team.save()
+
+
+class TestTeamGetDetailView(TestTeamView):
+    def test_get(self) -> None:
+        self.client.login(username='foobar', password='foobarbaz')
+
+        response = self.client.get(reverse('team-detail', args=[self.team.id]), secure=True)
+        self.assertEqual(response.status_code, 200)
+
+
+class TestTeamListView(TestTeamView):
+    def test_get(self) -> None:
+        self.client.login(username='foobar', password='foobarbaz')
+        response = self.client.get(reverse('team-list'), secure=True)
+        self.assertEqual(response.status_code, 200)
+
+
+class TestTeamToggleMembershipView(TestTeamView):
+    def test_toggle(self) -> None:
+        self.client.login(username='foobar', password='foobarbaz')
+
+        self.assertEqual(len(list(self.user.teams.all())), 0)
+
+        response = self.client.post(reverse('join-leave-team', args=[self.team.id]),
+                                    secure=True,
+                                    follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(list(self.user.teams.all())), 1)
+
+        response = self.client.post(reverse('join-leave-team', args=[self.team.id]),
+                                    secure=True,
+                                    follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(list(self.user.teams.all())), 0)
