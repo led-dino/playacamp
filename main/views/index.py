5091baa73187e1e6bd44e84d9b843c46ac3f5679
Jackie Luo <jackiehluo@gmail.com>
2018-06-12 09:46:26 -0700

    Redirect to login when unauthenticated and change colors

--- main/views/index.py
+++ main/views/index.py
@@ -1,10 +1,11 @@
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.views import login, logout
 from django.shortcuts import render, redirect
 
 
 def get(request: HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated:
-        return render(request, 'index.html')
+        return redirect('login')
 
     if request.user.profile.is_verified_by_admin:
         return redirect('user-profile-list')
