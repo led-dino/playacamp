407a3de20c7a15a1d0ece8cd6bb60f0c8cc46659
Mark Hahnenberg <mhahnenb@gmail.com>
2018-06-12 17:42:48 -0700

    Add logging configuration to settings.py

--- playacamp/settings.py
+++ playacamp/settings.py
@@ -120,6 +120,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
